d57a371d7ec2b2351f22efaaa32b36efacb0dcbb
Emir Özbir <emirozbir@hotmail.com>
2016-12-01 21:58:24 +0200

    CRONTAB ADDED

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-
+gem 'whenever', '~> 0.9.4',:require=>false
 gem 'thor', '~> 0.19.1'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 # Use mysql as the database for Active Record
---
+++ config/schedule.rb
@@ -0,0 +1,21 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+set :output, {:error => '~/Desktop/watchers_of_quakes_db.error.log', :standard => '~/Desktop/watchers_of_quakes_db.standard.log'}
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+ every 1.minute do
+    command "ruby /Users/emirozbir/Desktop/watchers_of_quakes/scripts/parser.rb"
+ end
+
+# Learn more: http://github.com/javan/whenever
