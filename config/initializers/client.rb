0c64e3b1e42fba7db21cd52a8fc3249f54f35dd4
Ahmet Sina Ustem <asend.ustem@gmail.com>
2017-02-03 19:20:11 +0200

    Update client.rb

--- config/initializers/client.rb
+++ config/initializers/client.rb
@@ -1,2 +1,3 @@
 require 'mysql2'
-client = Mysql2::Client.new(:host=>"localhost",:username=>"root",:password=>"root",:database=>"quakes", :socket =>"/tmp/mysql/mysql.sock")
+client = Mysql2::Client.new(:host=>"localhost",:username=>"root",:password=>"abcde",:database=>"watchers_of_quakes_development")
+# Use  :socket => "/tmp/mysql/mysql.sock" for socket settings. "/tmp/mysql/mysql.sock" is default
