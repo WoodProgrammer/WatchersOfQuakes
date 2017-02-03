b1d1756ea8ae7f52a3fff49e4cd64b910780b563
Ahmet Sina Ustem <asend.ustem@gmail.com>
2017-02-03 19:06:32 +0200

    Update sehir.rb
    interactive changed to client fetching as initializer from initiliazers/client.rb

--- app/models/sehir.rb
+++ app/models/sehir.rb
@@ -3,10 +3,9 @@ class <<self
   def date_detector
     require 'mysql2'
     city_name="Adana"
-    interactive=Mysql2::Client.new(:host=>"localhost",:username=>"root",:password=>"abcde",:database=>"watchers_of_quakes_development")
     file=open("/Users/emirozbir/Desktop/ruby/watchers_of_quakes/app/views/Adana.csv","a")
     file.puts("date,count")
-    data=interactive.query("SELECT * FROM cities WHERE city_name LIKE '%#{city_name}%'")
+    data=client.query("SELECT * FROM cities WHERE city_name LIKE '%#{city_name}%'")
     month=["November","December"]
     month.each do |month|
         data.each do |meta_data_city|
