152e01e1d41c850d36615c9acdcc2caad5d852b2
Ahmet Sina Ustem <asend.ustem@gmail.com>
2017-02-03 19:04:32 +0200

    Update quake.rb
    client variable fetching as initializer from initiliazers/client.rb file

--- app/models/quake.rb
+++ app/models/quake.rb
@@ -1,8 +1,6 @@
 class Quake < ApplicationRecord
 require 'terminal-notifier'
 require 'date'
-gem "mysql2"
-client = Mysql2::Client.new(:host=>"localhost",:username=>"root",:password=>"abcde",:database=>"watchers_of_quakes_development")
   class <<self
     def search(city_name)
   #  quake=Quake.all
